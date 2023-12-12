5656614f6054423e22abe290aa7356b311a5f9d4
Alessandro Sarchi <alessandro.sarchi02@gmail.com>
2023-12-12 15:15:35 +0100

    Search box funzionante

--- index.php
+++ index.php
@@ -21,180 +21,190 @@ if (!isset($_SESSION['email'])) {
     <link rel="stylesheet" href="style/myStyle.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/797b14a0a3.js" crossorigin="anonymous"></script>
     <link rel="icon" href="images/logo_bloonci_380x380.png" type="image/icon type">
-    <script src="javaScript/util.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-
+    <script src="https://kit.fontawesome.com/797b14a0a3.js" crossorigin="anonymous"></script>
+    <script src="javaScript/util.js"></script>
+    <script src="javaScript/autocomplete.js"></script>
 
 </head>
 
 <body style="background: #eeeeee;">
 
-    <!-- Navbar -->
-    <div class="container-fluid" style="margin-bottom: 40px;">
+    <div class=" z-0 position-absolute container-fluid p-0">
         <!-- Navbar -->
-        <nav class="navbar row" style="padding: 5px 20px; margin-bottom: 10px;">
-            <div class="col-12 d-flex justify-content-between align-items-center">
-                <div class="nav-left">
-                    <img src="images/scritta_bloonci_bianca.png" class="logo">
-                </div>
-
-                <div class="search-box mx-auto">
-                    <img src="images/search.png" alt="Search Icon">
-                    <input type="text" placeholder="Search">
-                </div>
-
-                <div class="nav-user-icon online">
-                    <span>
-                        <?php
-                        if (isset($_SESSION['email']) && isset($_SESSION['nome'])) {
-                            echo '<span class="nav-user-icon">' . $_SESSION['nome'] . ' ' . $_SESSION['cognome'] . '</span>';
-                        }
-                        ?>
-                    </span>
-                    <img src="images/notification.png" alt="Notification Icon">
-                    <img src="images/unkwownPhoto.jpeg" alt="User Photo">
-                </div>
-            </div>
-        </nav>
-    </div>
-
-    <!-- Sezione Centrale -->
-    <div class="container-fluid">
-        <div class="row">
-
-            <!-- Sidebar sinistra -->
-            <div class="col-md-3">
-                <div class="left-sidebar">
-
-                    <!--   Statistiche    -->
-                    <div class="sidebar-title">
-                        <h4>Statistiche</h4>
+        <div class="container-fluid" style="margin-bottom: 20px;">
+            <!-- Navbar -->
+            <nav class="navbar row" style="padding: 5px 20px; margin-bottom: 10px;">
+                <div class="col-12 d-flex justify-content-between align-items-center">
+                    <div class="nav-left">
+                        <img src="images/scritta_bloonci_bianca.png" class="logo">
                     </div>
 
-                    <div class="stats">
-                        <p> <i class="fa-solid fa-user"></i> Numero di amici:
-                            <span>
-                                <?php
-                                $emailUtente = $_SESSION['email'];
-                                $sql = "SELECT COUNT(*) AS numAmici
+                    <div class="search-container">
+                        <div class="search-box mx-auto">
+                            <img src="images/search.png" alt="Search Icon">
+                            <input type="text" id="input-box" placeholder="Search">
+                        </div>
+
+                        <div class="z-1 position-absolute">
+                            <div class="result-box">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="nav-user-icon online">
+                        <span>
+                            <?php
+                            if (isset($_SESSION['email']) && isset($_SESSION['nome'])) {
+                                echo '<span class="nav-user-icon">' . $_SESSION['nome'] . ' ' . $_SESSION['cognome'] . '</span>';
+                            }
+                            ?>
+                        </span>
+                        <img src="images/notification.png" alt="Notification Icon">
+                        <img src="images/unkwownPhoto.jpeg" alt="User Photo">
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+        <!-- Sezione Centrale -->
+        <div class="container-fluid">
+            <div class="row">
+
+                <!-- Sidebar sinistra -->
+                <div class="col-md-3">
+                    <div class="left-sidebar">
+
+                        <!--   Statistiche    -->
+                        <div class="sidebar-title">
+                            <h4>Statistiche</h4>
+                        </div>
+
+                        <div class="stats">
+                            <p> <i class="fa-solid fa-user"></i> Numero di amici:
+                                <span>
+                                    <?php
+                                    $emailUtente = $_SESSION['email'];
+                                    $sql = "SELECT COUNT(*) AS numAmici
                                         FROM amicizia
                                         WHERE emailRichiedente = '$emailUtente' 
                                         OR emailRicevitore = '$emailUtente'";
-                                $result = $cid->query($sql);
-                                $row = $result->fetch_assoc();
+                                    $result = $cid->query($sql);
+                                    $row = $result->fetch_assoc();
 
-                                echo $row['numAmici'];
-                                ?>
-                            </span>
-                        </p>
-                        <p> <i class="fa-solid fa-chart-line"></i> Indice di Rispettabilità:
-                            <span>
-                                <?php echo $_SESSION['rispettabilità'] ?>
-                            </span>
-                        </p>
+                                    echo $row['numAmici'];
+                                    ?>
+                                </span>
+                            </p>
+                            <p> <i class="fa-solid fa-chart-line"></i> Indice di Rispettabilità:
+                                <span>
+                                    <?php echo $_SESSION['rispettabilità'] ?>
+                                </span>
+                            </p>
+
+                            <!-- 
                         <p> <i class="fa-solid fa-square-plus"></i> Post nell'ultimo mese:
                             <span>23</span>
                         </p>
                         <p> <i class="fa-regular fa-heart"></i> Like nell'ultimo mese:
                             <span>234</span>
                         </p>
+                        -->
+                        </div>
+
+                        <hr class="separator">
+
+                        <!--  Amici  -->
+                        <div class="sidebar-title">
+                            <h4>Amici</h4>
+                        </div>
+
+                        <?php include "backEnd/friendList.php"; ?>
                     </div>
-
-
-
-                    <hr class="separator">
-
-
-                    <!--  Amici  -->
-
-                    <div class="sidebar-title">
-                        <h4>Amici</h4>
-                    </div>
-
-                    <?php include "backEnd/friendList.php"; ?>
                 </div>
-            </div>
 
-            <!-- Main Content -->
-            <div class="col-md-6">
-                <div class="main-content">
-                    <div class="write-post-container" style="height: 50%;">
-                        <div class="user-profile">
-                            <img src="images/unkwownPhoto.jpeg">
-                            <div>
-                                <p>Alessandro Sarchi</p>
-                                <small>Public <i class="fa-solid fa-earth-americas"></i></small>
+                <!-- Main Content -->
+                <div class="col-md-6">
+                    <div class="main-content">
+                        <div class="write-post-container" style="height: 50%;">
+                            <div class="user-profile">
+                                <img src="images/unkwownPhoto.jpeg">
+                                <div>
+                                    <p>Alessandro Sarchi</p>
+                                    <small>Public <i class="fa-solid fa-earth-americas"></i></small>
+                                </div>
+                            </div>
+
+                            <div class="post-input-container">
+                                <textarea rows="1" id="autoHeightTextarea"
+                                    placeholder="Cos'hai in mente in questo momento?" oninput="autoResize()"></textarea>
                             </div>
                         </div>
 
-                        <div class="post-input-container">
-                            <textarea rows="1" id="autoHeightTextarea" placeholder="Cos'hai in mente in questo momento?"
-                                oninput="autoResize()"></textarea>
-                        </div>
-                    </div>
+                        <div class="post-container">
+                            <div class="user-profile">
+                                <img src="images/unkwownPhoto.jpeg">
+                                <div>
+                                    <p>Marco Abbiati</p>
+                                    <small>25 Luglio 2017, 13:45</small>
+                                </div>
+                            </div>
+                            <p class="post-text">Ciao a tutti, come va oggi? per me molto bene dato che l'Inter ha
+                                perso
+                                lol
+                            </p>
+                            <img src="images/feed-image-1.png" class="post-img">
 
-                    <div class="post-container">
-                        <div class="user-profile">
-                            <img src="images/unkwownPhoto.jpeg">
-                            <div>
-                                <p>Marco Abbiati</p>
-                                <small>25 Luglio 2017, 13:45</small>
+                            <!-- Utilizzo della griglia di Bootstrap per organizzare i like e i commenti -->
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <!-- Sidebar destra -->
+                <div class="col-md-3">
+                    <div class="right-sidebar">
+
+                        <!--   Compleanni    -->
+                        <div class="sidebar-title">
+                            <h4>Compleanni</h4>
+                        </div>
+
+                        <div class="birthday">
+                            <div class="left-birthday">
+                                <h3>8</h3>
+                                <span>Dicembre</span>
+                            </div>
+                            <div class="right-birthday">
+                                <h4>Sofia Buda</h4>
+                                <p> <i class="fa-solid fa-cake-candles"></i> compie 22 anni</p>
                             </div>
                         </div>
-                        <p class="post-text">Ciao a tutti, come va oggi? per me molto bene dato che l'Inter ha perso lol
-                        </p>
-                        <img src="images/feed-image-1.png" class="post-img">
 
-                        <!-- Utilizzo della griglia di Bootstrap per organizzare i like e i commenti -->
-
-                    </div>
-
-                </div>
-            </div>
-
-            <!-- Sidebar destra -->
-            <div class="col-md-3">
-                <div class="right-sidebar">
-
-                    <!--   Compleanni    -->
-                    <div class="sidebar-title">
-                        <h4>Compleanni</h4>
-                    </div>
-
-                    <div class="birthday">
-                        <div class="left-birthday">
-                            <h3>8</h3>
-                            <span>Dicembre</span>
+                        <div class="birthday">
+                            <div class="left-birthday">
+                                <h3>25</h3>
+                                <span>Dicembre</span>
+                            </div>
+                            <div class="right-birthday">
+                                <h4>Gesù da Nazaret</h4>
+                                <p> <i class="fa-solid fa-cake-candles"></i> compie 2023 anni</p>
+                            </div>
                         </div>
-                        <div class="right-birthday">
-                            <h4>Sofia Buda</h4>
-                            <p> <i class="fa-solid fa-cake-candles"></i> compie 22 anni</p>
+
+                        <hr class="separator">
+
+
+                        <!--  Richieste di Amicizia  -->
+
+                        <div class="sidebar-title">
+                            <h4>Richieste di Amicizia</h4>
                         </div>
+
+                        <?php include "backEnd/friendRequests.php"; ?>
                     </div>
-
-                    <div class="birthday">
-                        <div class="left-birthday">
-                            <h3>25</h3>
-                            <span>Dicembre</span>
-                        </div>
-                        <div class="right-birthday">
-                            <h4>Gesù da Nazaret</h4>
-                            <p> <i class="fa-solid fa-cake-candles"></i> compie 2023 anni</p>
-                        </div>
-                    </div>
-
-                    <hr class="separator">
-
-
-                    <!--  Richieste di Amicizia  -->
-
-                    <div class="sidebar-title">
-                        <h4>Richieste di Amicizia</h4>
-                    </div>
-
-                    <?php include "backEnd/friendRequests.php"; ?>
                 </div>
             </div>
         </div>
---
+++ javaScript/autocomplete.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    let availableKeywords = [];
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "backEnd/getNames.php?q=", true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            availableKeywords = JSON.parse(xhr.responseText);
+        }
+    };
+    xhr.send();
+
+    const resultBox = document.querySelector(".result-box");
+    const inputBox = document.getElementById("input-box");
+
+
+    inputBox.onkeyup = function(){
+        let result = [];
+        let input = inputBox.value;
+
+
+        if (input.length) {
+            result = availableKeywords.filter((keyword)=>{
+                return keyword.toLowerCase().includes(input.toLowerCase());
+            });
+            console.log(result);
+            display(result)
+        } else {
+            resultBox.innerHTML = '';
+        }
+
+
+    };
+
+    function display(result) {
+        const content = result.map((list)=>{
+            return "<li>" + list + "</li>";
+        });
+
+        resultBox.innerHTML = "<ul>" + content.join('') + "</ul>";
+    }
+});
