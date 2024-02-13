e380a4a18489297d366bb01b2e57cec1cb43356e
Alessandro Sarchi <alessandro.sarchi02@gmail.com>
2024-02-13 21:18:07 +0100

    Corretto errori interni

--- javaScript/gestioneModifiche.js
+++ javaScript/gestioneModifiche.js
@@ -20,25 +20,6 @@ $(document).ready(function () {
     });
 });
 
-$(document).ready(function () {
-    $('#salvaModificheDataNascitaBtn').on('click', function () {
-        var nuovoOrientamento = $('#birthModal').val();
-        $.ajax({
-            url: '../backEnd/salvaModificheDataNascita.php',
-            method: 'POST',
-            data: {
-                orientamento: nuovoOrientamento
-            },
-            success: function (response) {
-                console.log(response);
-                location.reload();;
-            },
-            error: function (xhr, status, error) {
-                console.error(error);
-            }
-        });
-    });
-});
 
 $(document).ready(function () {
     $('#salvaModificheOrientBtn').on('click', function () {
@@ -59,3 +40,25 @@ $(document).ready(function () {
         });
     });
 });
+
+$(document).ready(function () {
+    $('#salvaModificheDataNascitaBtn').on('click', function () {
+        var nuovaData = $('#birthModal').val();
+        console.log("Nuova data di nascita:", nuovaData); // Aggiungi questo console.log per debug
+        $.ajax({
+            url: '../backEnd/salvaModificheDataNascita.php',
+            method: 'POST',
+            data: {
+                dataNascita: nuovaData // Assicurati che il nome del campo corrisponda al valore che stai inviando
+            },
+            success: function (response) {
+                console.log(response);
+                location.reload();
+            },
+            error: function (xhr, status, error) {
+                console.error(error);
+            }
+        });
+    });
+});
+
