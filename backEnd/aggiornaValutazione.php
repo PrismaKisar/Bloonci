5a52158aa5b0b29ea057fe450a3606c483f3a549
Alessandro Sarchi <alessandro.sarchi02@gmail.com>
2024-02-18 02:25:39 +0100

    Introdotto prima parte dell'algoritmo di rispettabilità

--- backEnd/aggiornaValutazione.php
+++ backEnd/aggiornaValutazione.php
@@ -35,6 +35,48 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         VALUES ('$newID', '$emailUtenteLoggato', '$emailMessaggio', ' $timestampMessaggio', '$valutazione')";
         $result = $cid->query($query);
     }
+    $query = "SELECT AVG(valutazione) AS valore_medio
+    FROM valuta
+    WHERE emailMessaggio = '$emailMessaggio'";
+    $result = $cid->query($query);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $valoreMedio = (int) $row["valore_medio"] - 4;
+    }
+    var_dump($valoreMedio);
+
+    if ($valoreMedio == 3) {
+        $nuovaRispettabilità = 10;
+    } elseif ($valoreMedio >= 2.4) {
+        $nuovaRispettabilità = 9;
+    } elseif ($valoreMedio >= 1.8) {
+        $nuovaRispettabilità = 8;
+    } elseif ($valoreMedio >= 1.2) {
+        $nuovaRispettabilità = 7;
+    } elseif ($valoreMedio >= 0.6) {
+        $nuovaRispettabilità = 6;
+    } elseif ($valoreMedio >= 0) {
+        $nuovaRispettabilità = 5;
+    } elseif ($valoreMedio >= -0.6) {
+        $nuovaRispettabilità = 4;
+    } elseif ($valoreMedio >= -1.2) {
+        $nuovaRispettabilità = 3;
+    } elseif ($valoreMedio >= -1.8) {
+        $nuovaRispettabilità = 2;
+    } elseif ($valoreMedio >= -2.4) {
+        $nuovaRispettabilità = 1;
+    } elseif ($valoreMedio >= -3) {
+        $nuovaRispettabilità = 0;
+    }
+
+    $query = "UPDATE utente SET rispettabilità = '$nuovaRispettabilità' WHERE email = '$emailMessaggio'";
+    var_dump($query);
+    if ($cid->query($query) === TRUE) {
+        echo "Il valore di rispettabilità dell'utente con email $emailMessaggio è stato aggiornato con successo.";
+    } else {
+        echo "Errore durante l'aggiornamento del valore di rispettabilità dell'utente: " . $cid->error;
+    }
 } else {
     echo json_encode("non tanto bene");
 }
