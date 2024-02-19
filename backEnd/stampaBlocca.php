31afa22da7f667c356314ac34b8219e9b21ce8cc
Alessandro Sarchi <alessandro.sarchi02@gmail.com>
2024-02-19 23:29:57 +0100

    refactoring

--- backEnd/stampaBlocca.php
+++ backEnd/stampaBlocca.php
@@ -5,13 +5,21 @@ session_start();
 $emailUtenteLoggato = $_SESSION['email'];
 $emailAmico = $_SESSION['emailBacheca'];
 
+// Query per verificare se l'utente loggato è un amministratore
 $query = "SELECT amministratore FROM utente WHERE email = '$emailUtenteLoggato'";
+
+// Query per ottenere l'informazione sul blocco dell'amico
 $query2 = "SELECT bloccante FROM utente WHERE email = '$emailAmico'";
+
+// Esegui le query per verificare se l'utente loggato è un amministratore e ottenere l'informazione sul blocco dell'amico
+$result = $cid->query($query);
 $result2 = $cid->query($query2);
 
-$result = $cid->query($query);
+// Estrai i risultati delle query
 $row = $result->fetch_assoc();
 $row2 = $result2->fetch_assoc();
+
+// Se l'utente è un amministratore e l'amico non è bloccato, visualizza il pulsante di blocco
 if ($row['amministratore'] == 1 && is_null($row2['bloccante'])) {
     echo <<<END
         <div class="logout">
--- backEnd/stampaSblocca.php
+++ backEnd/stampaSblocca.php
@@ -5,13 +5,19 @@ session_start();
 $emailUtenteLoggato = $_SESSION['email'];
 $emailAmico = $_SESSION['emailBacheca'];
 
+// Query per verificare se l'utente loggato è un amministratore
 $query = "SELECT amministratore FROM utente WHERE email = '$emailUtenteLoggato'";
+
+// Query per ottenere l'informazione sul blocco dell'amico
 $query2 = "SELECT bloccante FROM utente WHERE email = '$emailAmico'";
 $result2 = $cid->query($query2);
 
+// Esegui la query per verificare se l'utente loggato è un amministratore
 if ($result = $cid->query($query)) {
     $row = $result->fetch_assoc();
     $row2 = $result2->fetch_assoc();
+
+    // Se l'utente è un amministratore e l'amico è bloccato, visualizza il pulsante di sblocco
     if ($row['amministratore'] == 1 && !is_null($row2['bloccante'])) {
         echo <<<END
             <div class="logout">
