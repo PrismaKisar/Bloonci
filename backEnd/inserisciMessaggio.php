38bc4a446211bd7f0d525d322f863c5a8556f21c
Alessandro Sarchi <alessandro.sarchi02@gmail.com>
2024-02-19 13:07:25 +0100

    Ora anche il testo viene inserito correttamente

--- backEnd/inserisciMessaggio.php
+++ backEnd/inserisciMessaggio.php
@@ -6,57 +6,57 @@ if (!isset($_SESSION['email'])) {
     echo "Errore: Utente non autenticato";
     exit();
 }
-var_dump($_POST);
 $emailUtenteLoggato = $_SESSION['email'];
 
-// Directory di destinazione per l'utente loggato
-$targetDirectory = "../images/$emailUtenteLoggato/";
+if (!is_null($_FILES["file"]["name"])) {
+    // Directory di destinazione per l'utente loggato
+    $targetDirectory = "../images/$emailUtenteLoggato/";
 
-// Crea la directory se non esiste
-if (!is_dir($targetDirectory)) {
-    if (!mkdir($targetDirectory, 0755, true)) { // Imposta i permessi della nuova directory
-        echo "Errore: Impossibile creare la directory.";
+    // Crea la directory se non esiste
+    if (!is_dir($targetDirectory)) {
+        if (!mkdir($targetDirectory, 0755, true)) { // Imposta i permessi della nuova directory
+            echo "Errore: Impossibile creare la directory.";
+            exit();
+        }
+    }
+
+    $nome = basename($_FILES["file"]["name"]);
+    $targetFile = $targetDirectory . $nome;
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+    // Controlli sull'immagine
+    if (!getimagesize($_FILES["file"]["tmp_name"])) {
+        echo "Il file non è un'immagine.";
+        $uploadOk = 0;
         exit();
     }
-}
-var_dump($_FILES["file"]);
-$nome = basename($_FILES["file"]["name"]);
-$targetFile = $targetDirectory . $nome;
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
-// Controlli sull'immagine
-if (!getimagesize($_FILES["file"]["tmp_name"])) {
-    echo "Il file non è un'immagine.";
-    $uploadOk = 0;
-    exit();
-}
+    if (file_exists($targetFile)) {
+        echo "Il file esiste già.";
+        $uploadOk = 0;
+        exit();
+    }
 
-if (file_exists($targetFile)) {
-    echo "Il file esiste già.";
-    $uploadOk = 0;
-    exit();
-}
+    $allowedFormats = array("jpg", "png", "jpeg", "gif");
+    if (!in_array($imageFileType, $allowedFormats)) {
+        echo "Sono consentiti solo file JPG, JPEG, PNG e GIF.";
+        $uploadOk = 0;
+        exit();
+    }
 
-$allowedFormats = array("jpg", "png", "jpeg", "gif");
-if (!in_array($imageFileType, $allowedFormats)) {
-    echo "Sono consentiti solo file JPG, JPEG, PNG e GIF.";
-    $uploadOk = 0;
-    exit();
-}
-
-if ($uploadOk == 0) {
-    echo "Il file non è stato caricato.";
-    exit();
-} else {
-    if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile)) {
-        echo "Il file " . htmlspecialchars(basename($_FILES["file"]["name"])) . " è stato caricato.";
+    if ($uploadOk == 0) {
+        echo "Il file non è stato caricato.";
+        exit();
     } else {
-        echo "Si è verificato un errore durante il caricamento del file.";
-        exit();
+        if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile)) {
+            echo "Il file " . htmlspecialchars(basename($_FILES["file"]["name"])) . " è stato caricato.";
+        } else {
+            echo "Si è verificato un errore durante il caricamento del file.";
+            exit();
+        }
     }
 }
-
 
 $testo = $_POST['testo'];
 $tipo = $_POST['tipo'];
